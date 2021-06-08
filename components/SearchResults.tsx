15e3f5b34142d8186c72b69555192070890c62d3
Bruno Fray <brunofray@gmail.com>
2021-06-08 20:20:17 -0300

    Memo

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -20,3 +20,17 @@ export function SearchResults({ results }: SearchResultsProps) {
     </div>
   );
 }
+
+/**
+ * 1. Criar uma nova versão do componente
+ * 2. Comparar com a versão anterior
+ * 3. Se houverem alterações, vai atualizar o que alterou
+ */
+
+/**
+ * Utilização do memo
+ * 1. Pure Functional Components
+ * 2. Renders too often
+ * 3. Re-renders with same props
+ * 4. Medium to big size
+ */
